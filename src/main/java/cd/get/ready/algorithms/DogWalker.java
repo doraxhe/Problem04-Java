b2cb49cf50fe479c8b91e9382771706bc1f49d6a
doraxhe <doraapplication@gmail.com>
2020-10-10 18:29:39 -0400

    Finished.

--- src/main/java/cd/get/ready/algorithms/DogWalker.java
+++ src/main/java/cd/get/ready/algorithms/DogWalker.java
@@ -1,8 +1,30 @@
 package cd.get.ready.algorithms;
 
+import java.util.*;
+
 public class DogWalker {
     public int numberOfWalksCalculator(int[] dogSizes){
-        int numberOfWalks = -1;
+
+        int numberOfWalks = 0;
+        Map<Integer, Integer> hMap  = new HashMap<>();
+
+        for (int i = 0; i < dogSizes.length; i++) {
+            if (!hMap.containsKey(dogSizes[i])) {
+                hMap.put(dogSizes[i], 1);
+            }
+            else {
+                hMap.put(dogSizes[i], hMap.get(dogSizes[i]) + 1);
+            }
+        }
+
+        Collection<Integer> hMapValues = hMap.values();
+        Integer[] valuesArray = hMapValues.toArray(new Integer[hMapValues.size()]);
+
+        for (int j = 0; j < valuesArray.length; j++) {
+            int additionalWalks = ((valuesArray[j] + 1) / 2);
+            numberOfWalks += additionalWalks;
+        }
+
         return numberOfWalks;
     }
 }
